cb08b18a271c4db8d216993eb7e638fb72834fff
PaolaSoares <soaresgpaola@gmail.com>
2023-08-08 11:15:35 -0300

    atualizando

--- src/main.js
+++ src/main.js
@@ -1,6 +1,36 @@
 import { example } from './data.js';
-// import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+import countries from './data/countries/countries.js';
+import data from './data/countries/countries.js';
 
-console.log(example, data);
+const countriesData = data.countriesData;
+
+const listCoutry = document.getElementById('#root')
+
+countriesData.forEach(country => {
+    const item = document.createElement('li');
+    item.textContent = `
+    País: ${countries.name}
+    
+    `
+    listCoutry.appendChild(item)
+});
+
+// Cada um contém informações sobre:
+
+// - Seu nome comum e oficial.
+// - Su _Top Level Domain_ (tld), a extensão que utiliza na internet, por
+//   ejemplo o de Perú é `.pe` e o de Brasil é `.br`.
+// - Si se ha independente o no.
+// - Su(s) Capital(es).
+// - La _subregión_ a la que pertenece.
+// - Los idiomas falados neste país.
+// - Los países con los que limita.
+// - Sua área total (em metros cuadrados).
+// - Um emoji com sua respectiva bandera.
+// - A população total.
+// - Su [coeficiente de gini](https://es.wikipedia.org/wiki/Coeficiente_de_Gini)
+//   (do ano mais recente do qual se tem dados).
+// - A nomenclatura utilizada pela FIFA para identificar o país.
+// - Su huso horario.
+// - El continente al cual pertenece.
+// - E por último, imagens e descrição de sua bandeira.
