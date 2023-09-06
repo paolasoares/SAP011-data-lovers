da1b5c1accb4c4cb0d7fccd73049f5f2453cf8af
PaolaSoares <soaresgpaola@gmail.com>
2023-09-06 02:24:33 -0300

    Calculo Agregado

--- src/data.js
+++ src/data.js
@@ -26,3 +26,8 @@ export const filtraPaises = (countries, termoDePesquisa) => {
   });
   return paisesFiltrados;
 }
+
+
+//calculo agregado ok
+//limpeza do css
+//
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import data from './data/countries/countries.js';
-import { sortCountries, filtraPaises } from './data.js';
+import { sortCountries, filtraPaises} from './data.js';
 
 const countries = data.countries;
 const listCoutry = document.getElementById('root')
@@ -10,7 +10,6 @@ function renderCountries(countries) {
         <article class="card">
             <h2>País: ${country.name.common}</h2>              
             <img src="${country.flags.png}">               
-            <p>Fuso horário: ${country.timezones}</p>
             <p> Continente: ${country.continents}</p>
             <p> População: ${country.population}</p>
         </article>
@@ -41,31 +40,98 @@ pesquisa.addEventListener('input', () => {
   console.log(paisesFiltrados)
 
   renderCountries(paisesFiltrados);
-})
+});
+
+function calcularMediaPopulacaoPorContinente(countries) {
+  const populacaoPorContinente = {};
+
+  countries.forEach((country) => {
+    const continentes = country.continents.map((cont) => cont.toLowerCase());
+
+    continentes.forEach((continente) => {
+      if (!populacaoPorContinente[continente]) {
+        populacaoPorContinente[continente] = {
+          totalPopulacao: 0,
+          numeroPaises: 0,
+        };
+      }
+
+      populacaoPorContinente[continente].totalPopulacao += country.population;
+      populacaoPorContinente[continente].numeroPaises += 1;
+    });
+  });
+
+  // Calcular a média de população por continente
+  const mediaPopulacaoPorContinente = {};
+
+  for (const continente in populacaoPorContinente) {
+    const totalPopulacao = populacaoPorContinente[continente].totalPopulacao;
+    const numeroPaises = populacaoPorContinente[continente].numeroPaises;
+    mediaPopulacaoPorContinente[continente] = totalPopulacao / numeroPaises;
+  }
+
+  return mediaPopulacaoPorContinente;
+}
+
+const pesquisaContinenteInput = document.getElementById("pesquisarContinente");
+
+pesquisaContinenteInput.addEventListener("input", () => {
+  const termoDePesquisa = pesquisaContinenteInput.value.toLowerCase();
+
+  // Filtrar os países pelo termo de pesquisa
+  const paisesFiltrados = countries.filter(countries => {
+    const continentes = countries.continents.map(cont => cont.toLowerCase());
+    return continentes.some(cont => cont.includes(termoDePesquisa));
+  });
+
+  // Verificar se há resultados de pesquisa antes de exibi-los
+  if (paisesFiltrados.length > 0) {
+    // Calcular a média da população por continente para os países filtrados
+    const mediaPopulacaoPorContinente = calcularMediaPopulacaoPorContinente(paisesFiltrados);
+
+    // Exibir os resultados na interface do usuário (por exemplo, em um elemento HTML)
+    const resultadoContainer = document.getElementById("resultado");
+    resultadoContainer.innerHTML = "";
+
+    for (const continente in mediaPopulacaoPorContinente) {
+      const mediaPopulacao = mediaPopulacaoPorContinente[continente];
+      const resultadoItem = document.createElement("div");
+      resultadoItem.innerHTML = `Média de população em ${continente}: ${mediaPopulacao}`;
+      resultadoContainer.appendChild(resultadoItem);
+    }
+  } else {
+    // Se não houver resultados, você pode exibir uma mensagem informando que não há resultados.
+    const resultadoContainer = document.getElementById("resultado");
+    resultadoContainer.innerHTML = "Nenhum resultado encontrado.";
+  }
+});
+
+
+
+
+// const pesquisaContinenteInput = document.getElementById("pesquisarContinente");
+// pesquisaContinenteInput.addEventListener("input", () => {
+//   const termoDePesquisa = pesquisaContinenteInput.value.toLowerCase();
+
+//   // Filtrar os países pelo termo de pesquisa
+//   const paisesFiltrados = countries.filter(countries => {
+//     const continente = countries.continents.toLowerCase();
+//     return continente.includes(termoDePesquisa);
+//   });
+
+//   // Calcular a média da população por continente para os países filtrados
+//   const mediaPopulacaoPorContinente = calcularMediaPopulacao(paisesFiltrados);
+
+//   // Exibir os resultados na interface do usuário (por exemplo, em um elemento HTML)
+//   const resultadoContainer = document.getElementById("resultado");
+//   resultadoContainer.innerHTML = "";
+
+//   for (const continente in mediaPopulacaoPorContinente) {
+//     const mediaPopulacao = mediaPopulacaoPorContinente[continente];
+//     const resultadoItem = document.createElement("div");
+//     resultadoItem.innerHTML = `Média de população em ${continente}: ${mediaPopulacao}`;
+//     resultadoContainer.appendChild(resultadoItem);
+//   }
+// });
 
 renderCountries(countries);
-
-
-
-
-
-
-// Cada um contém informações sobre:
-
-// - Seu nome comum e oficial.
-// - Su _Top Level Domain_ (tld), a extensão que utiliza na internet, por
-//   ejemplo o de Perú é `.pe` e o de Brasil é `.br`.
-// - Si se ha independente o no.
-// - Su(s) Capital(es).
-// - La _subregión_ a la que pertenece.
-// - Los idiomas falados neste país.
-// - Los países con los que limita.
-// - Sua área total (em metros cuadrados).
-// - Um emoji com sua respectiva bandera.
-// - A população total.
-// - Su [coeficiente de gini](https://es.wikipedia.org/wiki/Coeficiente_de_Gini)
-//   (do ano mais recente do qual se tem dados).
-// - A nomenclatura utilizada pela FIFA para identificar o país.
-// - Su huso horario.
-// - El continente al cual pertenece.
-// - E por último, imagens e descrição de sua bandeira.
