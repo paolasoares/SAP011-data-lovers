bfd9c8a34d9a8b5f3b2c4639567a3d751fdaad5c
Alycia <alyekaka@gmail.com>
2023-09-06 09:51:22 -0300

    pequenas mudanças

--- src/data.js
+++ src/data.js
@@ -14,6 +14,7 @@ export const sortCountries = (countries, sortOrder) => {
     return sorted.reverse();
   }
 }
+
 export const filtraPaises = (countries, termoDePesquisa) => {
   if (!termoDePesquisa) {
     return countries;
--- src/main.js
+++ src/main.js
@@ -10,17 +10,13 @@ function renderCountries(countries) {
         <article class="card">
             <h2>País: ${country.name.common}</h2>              
             <img src="${country.flags.png}">               
-            <p>Fuso horário: ${country.timezones}</p>
             <p> Continente: ${country.continents}</p>
             <p> População: ${country.population}</p>
-        </article>
-      </div>
-    `).join("");
-
-
-  listCoutry.innerHTML = items;
-}
-
+            </article>
+            </div>
+            `).join("");
+            listCoutry.innerHTML = items;
+          }
 
 const barraBuscar = document.getElementById("ordenaçao");
 if (barraBuscar) {
@@ -44,28 +40,3 @@ pesquisa.addEventListener('input', () => {
 })
 
 renderCountries(countries);
-
-
-
-
-
-
-// Cada um contém informações sobre:
-
-// - Seu nome comum e oficial.
-// - Su _Top Level Domain_ (tld), a extensão que utiliza na internet, por
-//   ejemplo o de Perú é `.pe` e o de Brasil é `.br`.
-// - Si se ha independente o no.
-// - Su(s) Capital(es).
-// - La _subregión_ a la que pertenece.
-// - Los idiomas falados neste país.
-// - Los países con los que limita.
-// - Sua área total (em metros cuadrados).
-// - Um emoji com sua respectiva bandera.
-// - A população total.
-// - Su [coeficiente de gini](https://es.wikipedia.org/wiki/Coeficiente_de_Gini)
-//   (do ano mais recente do qual se tem dados).
-// - A nomenclatura utilizada pela FIFA para identificar o país.
-// - Su huso horario.
-// - El continente al cual pertenece.
-// - E por último, imagens e descrição de sua bandeira.
